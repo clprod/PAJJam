f709d15bd56bb01348ca18ff9505d4ee19225404
cocosar07 <cocosar07370@gmail.com>
2018-07-22 16:35:08 +0200

    remove tower price instead of 10

--- scripts/construction_menu.gd
+++ scripts/construction_menu.gd
@@ -31,18 +31,19 @@ func open():
 
 func _on_proj_button_pressed():
 	place_tower(projectile_tower_base.instance())
+	get_node("/root/game").change_wood(-projectile_tower_price)
 	hide()
 	remove_tower_place()
 
 func _on_aoe_button_pressed():
 	place_tower(aoe_tower_base.instance())
+	get_node("/root/game").change_wood(-aoe_tower_price)
 	hide()
 	remove_tower_place()
 
 func place_tower(tower):
 	tower.position = tower_place.position
 	get_node("/root/game/towers").add_child(tower)
-	get_node("/root/game").change_wood(-10)
 	
 func remove_tower_place():
 	tower_place.queue_free()
