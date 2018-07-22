7c503ecb12b548671def15186c926b43ac6200ca
LAFIOSCA LEO p1609209 <leo.lafiosca@etu.univ-lyon1.fr>
2018-07-22 16:15:36 +0200

    Better rock projectiles

--- scripts/rock_projectile.gd
+++ scripts/rock_projectile.gd
@@ -3,11 +3,13 @@ extends "res://scripts/projectile.gd"
 func _ready():
 	get_node("explosions")
 	get_node("explosions").set_emitting(false)
-	get_node("explosions/Timer").wait_time = 1
+	get_node("explosions/Timer").wait_time = 0.18
 
 func on_hit():
-	get_node("explosions").set_emitting(true)
+	speed = 0
 	get_node("Sprite").set_visible(false)
+	get_node("CollisionShape2D").disabled = true
+	get_node("explosions").set_emitting(true)
 	get_node("explosions/Timer").start()
 
 
